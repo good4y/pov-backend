f8a3716716e7bb94247e63168203d82562fa2fda
kim-youngchul <kmosh1212@naver.com>
2025-02-14 22:28:48 +0900

    refactor: 줄거리 없는 영화 삭제 하도록 배치 작업 수정

--- src/main/java/net/pointofviews/movie/batch/release/TMDbMovieReleaseWriter.java
+++ src/main/java/net/pointofviews/movie/batch/release/TMDbMovieReleaseWriter.java
@@ -1,6 +1,7 @@
 package net.pointofviews.movie.batch.release;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.pointofviews.movie.domain.Movie;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TMDbMovieReleaseWriter implements ItemWriter<Movie> {
@@ -18,7 +20,7 @@ public class TMDbMovieReleaseWriter implements ItemWriter<Movie> {
     public void write(Chunk<? extends Movie> chunk) {
         String sql = "UPDATE movie SET released = ?, film_rating = ? WHERE id = ?";
 
-        List<Object[]> batchArgs = chunk.getItems().stream()
+        List<Object[]> batchArgs = chunk.getItems().parallelStream()
                 .map(movie -> new Object[]{
                         movie.getReleased(),
                         movie.getFilmRating().getTmdbCode(),
@@ -27,5 +29,6 @@ public class TMDbMovieReleaseWriter implements ItemWriter<Movie> {
                 .toList();
 
         jdbcTemplate.batchUpdate(sql, batchArgs);
+        log.info("저장된 아이템 갯수 {}", chunk.getItems().size());
     }
 }
